2d97c636e7945d70b7155a1254ac6c9097d1b37c
nikfrankson <nikfrankson@mail.fresnostate.edu>
2019-11-07 17:37:03 -0800

    Exam 1 finished branch

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -12,7 +12,7 @@ import { Http } from '@angular/http';
 export class OrdersComponent implements OnInit {
 
   orders: Array<any> = [];
-
+  name: '';
   constructor(
     private router: Router,
     private flexModal: FlexModalService,
@@ -24,4 +24,64 @@ export class OrdersComponent implements OnInit {
 
   }
 
+
+  calculate() {
+    if (this.name === '') {
+      alert('Name Must not to empty!');
+    } else {
+      console.log('name', this.name);
+    }
+    if (this.name.indexOf(-1) === ',') {
+      alert('Must have a comma!');
+    } else {
+      console.log('name', this.name);
+    }
+  }
+  addItem(item: string) {
+    switch (item) {
+      case 'Android':
+        this.orders.unshift({
+          'pid': '1',
+          'image': 'assets/sm_android.jpeg',
+          'description': 'Android',
+          'price': 150.00,
+          'quantity': 1
+        });
+        break;
+      case 'IPhone':
+        this.orders.unshift({
+          'pid': '2',
+          'image': 'assets/sm_iphone.jpeg',
+          'description': 'IPhone',
+          'price': 200.00,
+          'quantity': 1
+        });
+        break;
+      case 'Windows Phone':
+        this.orders.unshift({
+          'pid': '3',
+          'image': 'assets/sm_windows.jpeg',
+          'description': 'Windows Phone',
+          'price': 110.00,
+          'quantity': 1
+        });
+        break;
+    }
+  }
+  delete(index: number) {
+    this.orders.splice(index, 1);
+  }
+
+  clear() {
+    this.orders = [];
+  }
+
+  async readFile() {
+    const rows = await this.http.get('assets/orders.json').toPromise();
+    console.log('rows', rows.json());
+    this.orders = rows.json();
+    return rows.json();
+  }
+
 }
+
